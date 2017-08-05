9b7dda436110f8c6b721d41125533e81c6cc6f9d
kanga333 <e411z7t40w@gmail.com>
2017-08-05 23:25:31 +0900

    update python

--- tools/build_isupam.sh
+++ tools/build_isupam.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
-OS_TARGETS="linux darwin freebsd windows"
+OS_TARGETS="linux"
 
 for os in $OS_TARGETS; do
-  if [ "$os" == "windows" ]; then
-    GOOS=$os go build -ldflags "-s -w" -o bin/isupam_$os.exe ./cmd/isupam
-  else
-    GOOS=$os go build -ldflags "-s -w" -o bin/isupam_$os ./cmd/isupam
-  fi
+  GOOS=$os go build -ldflags "-s -w" -o bin/isupam_$os ./cmd/isupam
 done
